68b51984d70dcd6d8a603d026f9ca65e61b0916c
mmarbonillo <mbonillo95@gmail.com>
2018-01-31 13:41:54 +0100

    Ejercicio_02

---
+++ projects/Ejercicio_02.php
@@ -0,0 +1,18 @@
+<?php
+
+echo $_SERVER['SERVER_ADDR'];
+echo "<br>";
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo "<br>";
+echo $_SERVER['SERVER_SOFTWARE'];
+echo "<br>";
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo "<br>";
+echo $_SERVER['REMOTE_ADDR'];
+
+
+?>
