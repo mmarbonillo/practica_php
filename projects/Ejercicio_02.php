ce4a8740e1f351c942631a78250317a52f7e6001
mmarbonillo <mbonillo95@gmail.com>
2018-01-31 14:14:46 +0100

    ModEj2 y Ejercicio_03

--- projects/Ejercicio_02.php
+++ projects/Ejercicio_02.php
@@ -13,6 +13,20 @@ echo $_SERVER['HTTP_USER_AGENT'];
 echo "<br>";
 echo "<br>";
 echo $_SERVER['REMOTE_ADDR'];
+echo "<br>";
+echo "<br>";
+#Ver el contenido completo de un array
+print_r($_SERVER);
+echo "<br>";
+echo "<br>";
+#Ver el contenido completo de un array y el tipo de dato que es
+var_dump($_SERVER);
+echo "<br>";
+echo "<br>";
 
+foreach ($_SERVER as $key => $value) {
+    echo "$key : $value";
+    echo "<br>";
+}
 
 ?>
---
+++ projects/Ejercicio_03.php
@@ -0,0 +1,27 @@
+<?php
+print_r($GLOBALS);
+echo "<br>";
+echo "<br>";
+print_r($_SERVER);
+echo "<br>";
+echo "<br>";
+print_r($_GET);
+echo "<br>";
+echo "<br>";
+print_r($_POST);
+echo "<br>";
+echo "<br>";
+print_r($_FILES);
+echo "<br>";
+echo "<br>";
+print_r($_COOKIE);
+echo "<br>";
+echo "<br>";
+print_r($_SESSION);
+echo "<br>";
+echo "<br>";
+print_r($_REQUEST);
+echo "<br>";
+echo "<br>";
+print_r($_ENV);
+?>
