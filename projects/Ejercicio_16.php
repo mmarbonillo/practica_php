e16190f0ced67b2780b98dd39d3f5720e60b29fa
mmarbonillo <mbonillo95@gmail.com>
2018-02-12 14:44:44 +0100

    Ejercicio_16

---
+++ projects/Ejercicio_16.php
@@ -0,0 +1,63 @@
+<!DOCTYPE <html>
+<html>
+<head>
+    <meta charset="utf-8" /> 
+    <title>Monedas</title> 
+</head> 
+<body>
+
+<form method="post">
+<div>
+<label for="num_tmp">Número de temperaturass:</label>
+    <select name="num_tmp">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4" selected>4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+    </select>
+
+    <button type="submit">Enviar</button>
+
+</div> 
+
+
+<?php
+
+$numero_tmp = $_POST["num_tmp"];
+
+echo $numero_tmp;
+echo "<br/>";
+echo "<br/>";
+
+$temperaturas = array();
+
+//Le damos un valor a cada posicion del array
+for($i = 0; $i < $numero_tmp; $i++) {
+    $temperaturas[$i] = rand(1, 30);
+}
+
+print_r($temperaturas);
+echo "<br/>";
+echo "<br/>";
+
+$temp = 0.0;
+
+for ($i = 0; $i < $numero_tmp; $i++) {
+    $temp = $temp + $temperaturas[$i];
+}
+
+$media = $temp / $numero_tmp;
+
+print_r($media);
+
+
+?>
+
+</body> 
+</html>
