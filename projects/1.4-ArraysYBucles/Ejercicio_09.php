7be638ebac0219d9f57707c8b2330e55f9f67285
mmarbonillo <mbonillo95@gmail.com>
2018-02-14 20:48:45 +0100

    1.4 Ejercicio_09.php

---
+++ projects/1.4-ArraysYBucles/Ejercicio_09.php
@@ -0,0 +1,26 @@
+<!DOCTYPE <html>
+<html>
+<head>
+    <meta charset="utf-8" /> 
+    <title>Lista</title> 
+</head> 
+<body>
+
+
+<?php
+
+$lista = array("Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw");
+$long = count($lista);
+
+//Ordeno el array ascendentemente por clave
+ksort($lista);
+
+//Recorro el array ya ordenado para mostrarlo
+foreach ($lista as $clave => $valor) {
+    echo " La capital de $clave es $valor <br/><br/>";
+}
+
+?>
+
+</body> 
+</html>
